4ce00f0fc2f14095934cc05d7023937c97357387
Huynh Huu Duc <1712354@student.hcmus.edu.vn>
2021-01-07 14:59:57 +0700

    !index.jsx: Fix dead loop when change route

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -4,27 +4,56 @@ import {Redirect, Route, Switch} from "react-router-dom";
 import Login from "./userAuthentication/LoginPage";
 import {useAuth} from "./useAuth";
 import HomePage from "./userAuthentication/HomePage";
-import UserInfo from "./userInfo/UserInfo";
+import GameInfo from "./games/GameInfo";
+import PlayerInfo from "./games/PlayerInfo";
+import GameList from "./games/GamesList";
+import UserList from "./userInfo/UserList";
 
 export default function IndexComponent({props}) {
 
   const {isAuth} = useAuth();
+
+  const PrivateRoute = ({component: Component, auth, ...rest}) => (
+    <Route
+      {...rest}
+      render={props => {
+        if (auth === false)
+          return <Redirect to='/login'/>;
+        return <Component {...props}/>;
+      }}
+    />
+  );
+
+  const PrivateRoute1 = ({component: Component, auth, ...rest}) => (
+    <Route {...rest} render={(props) => {
+      if (auth === undefined)
+        return <h2>Loading...</h2>
+      if (auth === false)
+        return <Redirect to='/login'/>;
+      return <Component {...props}/>;
+    }}/>
+  )
+
   return (
     <Switch>
+      <PrivateRoute1 exact path="/" auth={isAuth}>
+        <HomePage/>
+      </PrivateRoute1>
       <Route exact path="/login">
-        {
-          !isAuth ?
-            <Login/> :
-            <Redirect to="/"/>
-        }
+        {isAuth ? <Redirect to="/"/> : <Login/>}
       </Route>
-      <Route path="/">
-        {
-          !isAuth ?
-            <Redirect to="/login"/> :
-            <HomePage/>
-        }
-      </Route>
+      <PrivateRoute exact path="/game" auth={isAuth}>
+        <GameList/>
+      </PrivateRoute>
+      <PrivateRoute path="/game/:gameId" auth={isAuth}>
+        <GameInfo/>
+      </PrivateRoute>
+      <PrivateRoute exact path="/user" auth={isAuth}>
+        <UserList/>
+      </PrivateRoute>
+      <PrivateRoute path="/user/:userId" auth={isAuth}>
+        <PlayerInfo/>
+      </PrivateRoute>
     </Switch>
   )
 }
